f0a49e7b638f75a30176f602b78c7a6779766ee6
guowei <1101893803@qq.com>
2017-01-07 02:11:43 +0800

    add framework
    Signed-off-by: guowei <1101893803@qq.com>

---
+++ av/media/libstagefright/Android.mk
@@ -0,0 +1,151 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include frameworks/av/media/libstagefright/codecs/common/Config.mk
+
+LOCAL_SRC_FILES:=                         \
+        ACodec.cpp                        \
+        AACExtractor.cpp                  \
+        AACWriter.cpp                     \
+        AMRExtractor.cpp                  \
+        AMRWriter.cpp                     \
+        AudioPlayer.cpp                   \
+        AudioSource.cpp                   \
+        AwesomePlayer.cpp                 \
+        CameraSource.cpp                  \
+        CameraSourceTimeLapse.cpp         \
+        ClockEstimator.cpp                \
+        CodecBase.cpp                     \
+        DataSource.cpp                    \
+        DataURISource.cpp                 \
+        DRMExtractor.cpp                  \
+        ESDS.cpp                          \
+        FileSource.cpp                    \
+        FLACExtractor.cpp                 \
+        HTTPBase.cpp                      \
+        JPEGSource.cpp                    \
+        MP3Extractor.cpp                  \
+        MPEG2TSWriter.cpp                 \
+        MPEG4Extractor.cpp                \
+        MPEG4Writer.cpp                   \
+        MediaAdapter.cpp                  \
+        MediaBuffer.cpp                   \
+        MediaBufferGroup.cpp              \
+        MediaCodec.cpp                    \
+        MediaCodecList.cpp                \
+        MediaCodecSource.cpp              \
+        MediaDefs.cpp                     \
+        MediaExtractor.cpp                \
+        http/MediaHTTP.cpp                \
+        MediaMuxer.cpp                    \
+        MediaSource.cpp                   \
+        MetaData.cpp                      \
+        NuCachedSource2.cpp               \
+        NuMediaExtractor.cpp              \
+        OMXClient.cpp                     \
+        OMXCodec.cpp                      \
+        OggExtractor.cpp                  \
+        SampleIterator.cpp                \
+        SampleTable.cpp                   \
+        SkipCutBuffer.cpp                 \
+        StagefrightMediaScanner.cpp       \
+        StagefrightMetadataRetriever.cpp  \
+        SurfaceMediaSource.cpp            \
+        ThrottledSource.cpp               \
+        TimeSource.cpp                    \
+        TimedEventQueue.cpp               \
+        Utils.cpp                         \
+        VBRISeeker.cpp                    \
+        WAVExtractor.cpp                  \
+        WVMExtractor.cpp                  \
+        XINGSeeker.cpp                    \
+        avc_utils.cpp                     \
+
+LOCAL_C_INCLUDES:= \
+        $(TOP)/frameworks/av/include/media/ \
+        $(TOP)/frameworks/av/include/media/stagefright/timedtext \
+        $(TOP)/frameworks/native/include/media/hardware \
+        $(TOP)/frameworks/native/include/media/openmax \
+        $(TOP)/external/flac/include \
+        $(TOP)/external/tremolo \
+        $(TOP)/external/openssl/include \
+        $(TOP)/external/libvpx/libwebm \
+        $(TOP)/system/netd/include \
+        $(TOP)/external/icu/icu4c/source/common \
+        $(TOP)/external/icu/icu4c/source/i18n \
+
+LOCAL_SHARED_LIBRARIES := \
+        libbinder \
+        libcamera_client \
+        libcutils \
+        libdl \
+        libdrmframework \
+        libexpat \
+        libgui \
+        libicui18n \
+        libicuuc \
+        liblog \
+        libmedia \
+        libnetd_client \
+        libopus \
+        libsonivox \
+        libssl \
+        libstagefright_omx \
+        libstagefright_yuv \
+        libsync \
+        libui \
+        libutils \
+        libvorbisidec \
+        libz \
+        libpowermanager
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_color_conversion \
+        libstagefright_aacenc \
+        libstagefright_matroska \
+        libstagefright_webm \
+        libstagefright_timedtext \
+        libvpx \
+        libwebm \
+        libstagefright_mpeg2ts \
+        libstagefright_id3 \
+        libFLAC \
+        libmedia_helper
+
+LOCAL_SHARED_LIBRARIES += \
+        libstagefright_enc_common \
+        libstagefright_avc_common \
+        libstagefright_foundation \
+        libdl
+
+LOCAL_CFLAGS += -Wno-multichar
+
+ifeq ($(EN_FFMPEG_EXTRACTOR),true)
+
+FFMPEG_PATH     := $(TOP)/hardware/samsung_slsi/slsiap/omx/codec/ffmpeg
+LOCAL_CPPFLAGS  += -DENABLE_FFMPEG_EXTRACTOR=1
+LOCAL_CFLAGS    += -D__STDC_CONSTANT_MACROS=1 -D__STDINT_LIMITS=1
+LOCAL_C_INCLUDES += $(FFMPEG_PATH)/include \
+                $(TOP)/hardware/samsung_slsi/slsiap/omx/extractor/ffmpeg \
+                $(LOCAL_PATH)/ffmpegExtractor
+
+LOCAL_STATIC_LIBRARIES += libNX_FFMpegExtractor
+
+LOCAL_LDFLAGS += \
+        -L$(FFMPEG_PATH)/libs   \
+        -lavutil-2.1.4          \
+        -lavcodec-2.1.4         \
+        -lavformat-2.1.4        \
+        -lavdevice-2.1.4        \
+        -lavfilter-2.1.4        \
+        -lavresample-2.1.4
+
+endif	#	EN_FFMPEG_EXTRACTOR
+
+LOCAL_MODULE:= libstagefright
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ av/media/libstagefright/wifi-display/Android.mk
@@ -0,0 +1,63 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	MediaReceiver.cpp               \
+	MediaSender.cpp                 \
+	Parameters.cpp                  \
+	rtp/RTPAssembler.cpp            \
+	rtp/RTPReceiver.cpp             \
+	rtp/RTPSender.cpp               \
+	SNTPClient.cpp                  \
+	TimeSyncer.cpp                  \
+	source/Converter.cpp            \
+	source/MediaPuller.cpp          \
+	source/PlaybackSession.cpp      \
+	source/RepeaterSource.cpp       \
+	source/TSPacketizer.cpp         \
+	source/WifiDisplaySource.cpp    \
+	VideoFormats.cpp                
+
+LOCAL_C_INCLUDES:= \
+        $(TOP)/frameworks/av/media/libstagefright \
+        $(TOP)/frameworks/native/include/media/openmax \
+        $(TOP)/frameworks/av/media/libstagefright/mpeg2ts \
+
+LOCAL_SHARED_LIBRARIES:= \
+        libbinder                       \
+        libcutils                       \
+        liblog                          \
+        libgui                          \
+        libmedia                        \
+        libstagefright                  \
+        libstagefright_foundation       \
+        libui                           \
+        libutils                        \
+
+LOCAL_MODULE:= libstagefright_wfd
+
+LOCAL_MODULE_TAGS:= optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+################################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+        wfd.cpp                 \
+
+LOCAL_SHARED_LIBRARIES:= \
+        libbinder                       \
+        libgui                          \
+        libmedia                        \
+        libstagefright                  \
+        libstagefright_foundation       \
+        libstagefright_wfd              \
+        libutils                        \
+        liblog                          \
+
+LOCAL_MODULE:= wfd
+
+include $(BUILD_EXECUTABLE)
